7e480c9ba602de5472e8a8a867ef78969795d21d
Rob Brinkman <rob.brinkman@gmail.com>
2022-04-01 17:55:12 +0200

    Working first version

--- dato.config.js
+++ dato.config.js
@@ -39,6 +39,11 @@ module.exports = (dato, root, i18n) => {
     });
   });
 
+  console.log(dato.site)
+
+
+
+  /*
   // Create a YAML data file to store global data about the site
   root.createDataFile('data/settings.yml', 'yaml', {
     name: dato.site.globalSeo.siteName,
@@ -55,41 +60,51 @@ module.exports = (dato, root, i18n) => {
     faviconMetaTags: toHtml(dato.site.faviconMetaTags),
     seoMetaTags: toHtml(dato.home.seoMetaTags)
   });
+*/
 
-  // Create a markdown file with content coming from the `about_page` item
-  // type stored in DatoCMS
-  root.createPost(`content/about.md`, 'yaml', {
+  root.createPost('content/index.md', 'yaml', {
     frontmatter: {
-      title: dato.aboutPage.title,
-      subtitle: dato.aboutPage.subtitle,
-      photo: dato.aboutPage.photo.url({ w: 800, fm: 'jpg', auto: 'compress' }),
-      seoMetaTags: toHtml(dato.aboutPage.seoMetaTags),
-      menu: { main: { weight: 100 } }
-    },
-    content: dato.aboutPage.bio
+      title: 'test'
+    }
   });
 
-  // Create a `work` directory (or empty it if already exists)...
-  root.directory('content/works', dir => {
-    // ...and for each of the works stored online...
-    dato.works.forEach((work, index) => {
-      // ...create a markdown file with all the metadata in the frontmatter
-      dir.createPost(`${work.slug}.md`, 'yaml', {
-        frontmatter: {
-          title: work.title,
-          coverImage: work.coverImage.url({ w: 450, fm: 'jpg', auto: 'compress' }),
-          image: work.coverImage.url({ fm: 'jpg', auto: 'compress' }),
-          detailImage: work.coverImage.url({ w: 600, fm: 'jpg', auto: 'compress' }),
-          excerpt: work.excerpt,
-          seoMetaTags: toHtml(work.seoMetaTags),
-          extraImages: work.gallery.map(item =>
-            item.url({ h: 300, fm: 'jpg', auto: 'compress' })
-          ),
-          weight: index
-        },
-        content: work.description
+  /*
+    // Create a markdown file with content coming from the `about_page` item
+    // type stored in DatoCMS
+    root.createPost(`content/about.md`, 'yaml', {
+      frontmatter: {
+        title: dato.aboutPage.title,
+        subtitle: dato.aboutPage.subtitle,
+        photo: dato.aboutPage.photo.url({ w: 800, fm: 'jpg', auto: 'compress' }),
+        seoMetaTags: toHtml(dato.aboutPage.seoMetaTags),
+        menu: { main: { weight: 100 } }
+      },
+      content: dato.aboutPage.bio
+    });
+    */
+  /*
+    // Create a `work` directory (or empty it if already exists)...
+    root.directory('content/works', dir => {
+      // ...and for each of the works stored online...
+      dato.works.forEach((work, index) => {
+        // ...create a markdown file with all the metadata in the frontmatter
+        dir.createPost(`${work.slug}.md`, 'yaml', {
+          frontmatter: {
+            title: work.title,
+            coverImage: work.coverImage.url({ w: 450, fm: 'jpg', auto: 'compress' }),
+            image: work.coverImage.url({ fm: 'jpg', auto: 'compress' }),
+            detailImage: work.coverImage.url({ w: 600, fm: 'jpg', auto: 'compress' }),
+            excerpt: work.excerpt,
+            seoMetaTags: toHtml(work.seoMetaTags),
+            extraImages: work.gallery.map(item =>
+              item.url({ h: 300, fm: 'jpg', auto: 'compress' })
+            ),
+            weight: index
+          },
+          content: work.description
+        });
       });
     });
-  });
+  */
 };
 
