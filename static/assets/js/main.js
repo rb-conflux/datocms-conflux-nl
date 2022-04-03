a018c01ef800d28e630f12be63befb151fe1fa95
Rob Brinkman <rob.brinkman@gmail.com>
2022-04-03 13:10:57 +0200

    Ugly integration of templates incoming

---
+++ static/assets/js/main.js
@@ -0,0 +1,25 @@
+
+/*
+| ==========================================================
+| Scroll To Top
+| ========================================================== */
+
+$(document).ready(function() {
+    'use strict';
+    // Scroll To Top
+    $('body').prepend('<div class="go-top"><span id="top"><img src="assets/img/scroll-to-top.svg" alt="top" /></span></div>');
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 500) {
+            $('.go-top').fadeIn(600);
+        } else {
+            $('.go-top').fadeOut(600);
+        }
+    });
+
+    $('#top').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 800);
+        return false;
+    });
+
+});
